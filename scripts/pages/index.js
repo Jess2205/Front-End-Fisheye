a582c66e7cc4639dd9f3cead520306fbcd632b4f
Isidore <jessica.muschke79@gmail.com>
2024-09-04 16:05:56 +0200

    Ajout coment

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,16 +1,20 @@
 
+// scripts/pages/index.js
 
- 
- // scripts/pages/index.js
+// Fonction pour récupérer les photographes depuis le fichier JSON
 
 async function getPhotographers() {
     try {
+        console.log('Début de la récupération des photographes.');
         const response = await fetch('/data/photographers.json');
+
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
+
         const data = await response.json();
         console.log('Données récupérées :', data);
+
         return { photographers: data.photographers };
     } catch (error) {
         console.error('Erreur lors de la récupération des photographes :', error);
@@ -19,11 +23,18 @@ async function getPhotographers() {
 
 }
 
-
+// Fonction pour afficher les données des photographes sur la page
 async function displayData(photographers) {
+    console.log('Début de l\'affichage des photographes.');
     const photographersSection = document.querySelector(".photographer_section");
 
     photographers.forEach((photographer) => {
+        console.log('Affichage du photographe :', photographer.name);
+
+        if (typeof photographerTemplate === 'undefined') {
+            console.error('La fonction photographerTemplate n\'est pas définie.');
+            return;
+        }
         // eslint-disable-next-line no-undef
         const photographerModel = photographerTemplate(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
@@ -31,11 +42,14 @@ async function displayData(photographers) {
     });
 }
 
+// Fonction d'initialisation, appelée au chargement de la page
 async function init() {
     // Récupère les datas des photographes
+    console.log('Initialisation de la page d\'accueil.');
     const { photographers } = await getPhotographers();
     displayData(photographers);
 }
 
+// Appel de la fonction d'initialisation
 init();
 
